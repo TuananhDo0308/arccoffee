1d2c031a1e797782d6ddf69192d42f68ef5d8c08
Ấn Trần <tquocan04@gmail.com>
2025-05-29 15:57:15 +0700

    fix: update quantity item in cart

--- src/app/api/user/cart/updateQuantity/route.ts
+++ src/app/api/user/cart/updateQuantity/route.ts
@@ -5,14 +5,14 @@ import { auth } from '@/auth';
 
 export const PUT = async (req: NextRequest) => {
     const session=await auth()
-    const token =session?.user?.token
+    const token =session?.user?.accessToken
 
     try {        
         const { searchParams } = new URL(req.url);
         const productId = searchParams.get('productId'); // Lấy productId từ query params
         const quantity = searchParams.get('Quantity'); // Lấy Quantity từ query params
         console.log(productId, quantity);
-        const response = await httpClient.patch({
+        const response = await httpClient.put({
             url: `${apiLinks.cart.updateQuantity}/${productId}?Quantity=${quantity}`, // Thay productId vào URL
             token: token, // Thay token nếu cần
           });
@@ -24,7 +24,7 @@ export const PUT = async (req: NextRequest) => {
         console.error('Error during get api:', error);
 
         if (axios.isAxiosError(error) && error.response) {
-            const errorMessage = error.response.data?.message || 'Failed to fetch bill details';
+            const errorMessage = error.response.data?.message || 'Failed to update quantity';
             return NextResponse.json(
                 { message: errorMessage },
                 { status: error.response.status }
